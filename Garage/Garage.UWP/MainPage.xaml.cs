abcebdeedf5092482bfa758fd4bb9ad2ed50d1fd
Dmitry Mikhaltchenkov <mikhaltchenkov@gmail.com>
2018-01-23 23:18:42 +0300

    Добавьте файлы проекта.

---
+++ Garage/Garage.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Garage.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new Garage.App());
+        }
+    }
+}
